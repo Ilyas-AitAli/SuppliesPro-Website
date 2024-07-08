65b6ba4d29f9c797fce3c92d42f7f07305eb3f70
Ilyas Ait Ali <ilyasgameraa@gmail.com>
2024-07-07 23:09:00 -0400

    Updates

---
+++ precache-manifest.0c9612812aaa899e72536af43f5e2ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ff1685349365c66dd9dba805f07352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eb824a13abfd990b866",
+    "url": "/static/css/main.eefe9d35.chunk.css"
+  },
+  {
+    "revision": "fd7c384d5db53334f9a9",
+    "url": "/static/js/2.63b585ac.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "2eb824a13abfd990b866",
+    "url": "/static/js/main.47ab6684.chunk.js"
+  },
+  {
+    "revision": "02558d794f1b309512ce",
+    "url": "/static/js/runtime~main.f27e42ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SuppliesPro-Website/precache-manifest.6b7d91aea558bc6892abcdb580002fdf.js"
+  "/precache-manifest.0c9612812aaa899e72536af43f5e2ffe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SuppliesPro-Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
